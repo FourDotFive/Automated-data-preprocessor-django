52a0e65686a70cad8a4edd01fb1c976cc243c238
FourDotFive <aleksandrs.jermakovs.0704@gmail.com>
2023-07-04 15:11:57 +0300

    table scrolling and data loader

--- website/preprocessor/file_handler.py
+++ website/preprocessor/file_handler.py
@@ -1,17 +1,21 @@
 import datetime
+import json
 import pandas as pd
 import os
 import csv
 from django.conf import settings
 
 
-def get_file_extension(uploaded_file):
-    return os.path.splitext(uploaded_file.name)[1]
+def get_file_name_and_extension(uploaded_file):
+    file_name, file_extension = os.path.splitext(uploaded_file.name)
+    if file_extension not in ['.csv', '.txt']:
+        raise ValueError('Invalid file type. Only .csv and .txt files are allowed.')
+    else:
+        return file_name, file_extension
 
 
 def get_delimiter(file):
-    # To reset the pointer to read from the file again.
-    file.seek(0)
+    file.seek(0)    # To reset the pointer to read from the file again.
     sample = file.read().decode('utf-8')
     sniffer = csv.Sniffer()
     dialect = sniffer.sniff(sample)
@@ -20,20 +24,32 @@ def get_delimiter(file):
 
 
 def read_file(file, delimiter: str = ';'):
-    file_extension = get_file_extension(file)
-    if file_extension != '.csv' and file_extension != '.txt':
-        raise ValueError('Invalid file type. Only .csv and .txt files are allowed.')
-    else:
-        try:
-            df = pd.read_csv(file, delimiter=delimiter)
-            return df
-        except pd.errors.EmptyDataError:
-            raise ValueError('No data in the file')
+    try:
+        df = pd.read_csv(file, delimiter=delimiter)
+        return df
+    except pd.errors.EmptyDataError:
+        raise ValueError('No data in the file')
 
 
-def get_df_head(df: pd.DataFrame, number_of_rows: int = 10):
+def read_file_from_temp(folder_name):
+    folder_dir = os.path.join(settings.MEDIA_ROOT, 'temp', folder_name)
+    json_path = os.path.join(folder_dir, 'data.json')
+
+    with open(json_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    extension = data['extension']
+    delimiter = data['delimiter']
+
+    file_path = os.path.join(folder_dir, 'file' + extension)
+    df = pd.read_csv(file_path, delimiter=delimiter)
+
+    return df
+
+
+def get_df_head_dict(df: pd.DataFrame, number_of_rows: int = 10):
     df_head = df.head(number_of_rows)
-    df_head = df_head.to_dict('index')
+    df_head = df_head.to_dict(orient='index')
     return df_head
 
 
@@ -45,22 +61,34 @@ def get_df_stats(df: pd.DataFrame):
 
 
 def get_file_name_and_size(file):
-    file_name = file.name
-    file_size = round(file.size / 1048576, 2)
-    return file_name, file_size
+    return round(file.size / 1048576, 2)
 
 
-def save_file_by_chunks(file):
+def save_file_and_info(file, name, extension, delimiter):
+    file.seek(0)
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S-%f")
-    file_name = timestamp + '-' + file.name
 
-    file_dir = os.path.join(settings.MEDIA_ROOT, 'temp')
-    os.makedirs(file_dir, exist_ok=True)
+    folder_name = timestamp + '-' + name
 
-    file_path = os.path.join(settings.MEDIA_ROOT, 'temp', file_name)
-    print(file_path)
+    folder_dir = os.path.join(settings.MEDIA_ROOT, 'temp', folder_name)
+    os.makedirs(folder_dir, exist_ok=False)
+
+    json_path = os.path.join(folder_dir, 'data.json')
+    file_path = os.path.join(folder_dir, 'file' + extension)
+
+    data = {
+        'file_initial_name': name,
+        'extension': extension,
+        'delimiter': delimiter
+    }
+
+    with open(json_path, 'w') as json_file:
+        json.dump(data, json_file)
 
     with open(file_path, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
-    return file_path
+
+    file.seek(0)
+
+    return folder_name
--- website/preprocessor/views.py
+++ website/preprocessor/views.py
@@ -2,7 +2,15 @@ import datetime
 from django.http import JsonResponse
 from django.shortcuts import render
 from .forms import FileUploadForm
-from .file_handler import read_file, get_df_head, get_delimiter, get_file_name_and_size, save_file_by_chunks
+from .file_handler import (
+    read_file,
+    get_df_head_dict,
+    get_delimiter,
+    get_file_name_and_size,
+    save_file_and_info,
+    get_file_name_and_extension,
+    read_file_from_temp,
+)
 
 
 def file_uploader_view(request):
@@ -10,9 +18,9 @@ def file_uploader_view(request):
         file_form = FileUploadForm(request.POST, request.FILES)
         if file_form.is_valid():
             file = request.FILES['file']
-            file_path = save_file_by_chunks(file)
-            print(file_path)
-            file_name, file_size = get_file_name_and_size(file)
+
+            file_name, file_extension = get_file_name_and_extension(file)
+            file_size = get_file_name_and_size(file)
 
             if_detect_delimiter = file_form.cleaned_data.get('if_detect_delimiter')
             if if_detect_delimiter:
@@ -25,27 +33,41 @@ def file_uploader_view(request):
             except ValueError as e:
                 return render(request, 'error.html', {'error_message': str(e)})
             else:
-                df_head_records_dict = get_df_head(df, number_of_rows=20)
-                column_names = df.columns.values
 
-                context = {
-                    'file_name': file_name,
-                    'file_size': file_size,
-                    'column_names': column_names,
-                    'df_head_records': df_head_records_dict,
-                }
-                return render(request, 'data_stats.html', context=context)
+                try:
+                    folder_name = save_file_and_info(
+                        file=file,
+                        name=file_name,
+                        extension=file_extension,
+                        delimiter=delimiter,
+                    )
+                except OSError:
+                    return render(request, 'error.html', {'error_message': 'Please connect with the support'})
+                else:
+                    df_head_records_dict = get_df_head_dict(df, number_of_rows=20)
+                    column_names = df.columns.values
+
+                    context = {
+                        'file_name': file_name,
+                        'file_size': file_size,
+                        'column_names': column_names,
+                        'df_head_records': df_head_records_dict,
+                        'folder_name': folder_name
+                    }
+                    return render(request, 'data_stats.html', context=context)
 
     file_form = FileUploadForm(request.POST)
     return render(request, 'uploader.html', context={'file_form': file_form})
 
 
 def more_data(request):
+    length = 20
+
     start = int(request.GET.get('start'))
-    length = 20  # Or whatever number of rows you want to send each time
-    file = request.FILES['file']
-    print(file)
-    df = read_file(file)
-    print(df)
-    next_rows = df[start:start+length].to_dict(orient='records')
+    folder_name = request.GET.get('folder_name')
+
+    df = read_file_from_temp(folder_name)
+
+    next_rows = df[start:start + length].to_dict(orient='index')
+
     return JsonResponse(next_rows, safe=False)
